96765d9d91e942af05c5819894a05513721fcbfc
Marino von Wattenwyl <m.v.wattenwyl@gmail.com>
2024-01-11 15:58:34 +0100

    [Emscripten] Change Emscripten version to 3.1.46

--- apps/app_demo_slproject/emscripten/AppDemoMainEmscripten.cpp
+++ apps/app_demo_slproject/emscripten/AppDemoMainEmscripten.cpp
@@ -552,6 +552,7 @@ int main(void)
     emscripten_set_beforeunload_callback(nullptr, emOnUnload);
 
     // HACK: Fixes to make this able to run in an <iframe>
+    // clang-format off
     MAIN_THREAD_EM_ASM({
         const canvas = document.querySelector("#canvas");
         
@@ -570,6 +571,7 @@ int main(void)
         canvas.addEventListener("touchend", event => event.preventDefault());
         canvas.addEventListener("touchmove", event => event.preventDefault());
     });
+    // clang-format on
 
     AppDemo::calibIniPath = "data/calibrations/";
 
