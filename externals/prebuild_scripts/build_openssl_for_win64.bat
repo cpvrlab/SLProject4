59427def26f94d35ea895ef56a824ae9860abb80
luc <luc.girod@bfh.ch>
2024-03-18 12:43:45 +0100

    fix error while updating to 3.2.1

--- externals/prebuild_scripts/build_openssl_for_win64.bat
+++ externals/prebuild_scripts/build_openssl_for_win64.bat
@@ -5,12 +5,11 @@
 
 @echo off
 
-::set OPENSSL_VERSION="OpenSSL_3_2_1"
-set VERSION="3.2.1"
+set OPENSSL_VERSION="openssl-3.2.1"
+::set VERSION="3.2.1"
 
 if "%1" == "" (
-    set VERSION="%1"
-    ::set OPENSSL_VERSION="%1"
+    set OPENSSL_VERSION="%1"
 )
 
 :: To enable downloading prebuilds copy 
@@ -19,28 +18,21 @@ set MAX_NUM_CPU_CORES=6
 set SLPROJECT_ROOT=%2
 set PREFIX=%cd%\..\prebuilt\win64_openssl
 
-::echo Building OpenSSL Version: %OPENSSL_VERSION%
-echo Building OpenSSL Version: %VERSION%
+echo Building OpenSSL Version: %OPENSSL_VERSION%
 echo Installation directory: %PREFIX%
 echo Using %MAX_NUM_CPU_CORES% cpu cores for build.
 
-::-----------------------------------------------------------------::
-::if not exist openssl (
-::    git clone https://github.com/openssl/openssl.git
-::) else (
-::    echo openssl already exists
-::)
-::cd openssl 
-::git checkout %OPENSSL_VERSION%
-::git pull origin %OPENSSL_VERSION%
-::
-::perl Configure VC-WIN64A --prefix=%PREFIX% --openssldir=%PREFIX%
+-----------------------------------------------------------------::
+if not exist openssl (
+    git clone https://github.com/openssl/openssl.git
+) else (
+    echo openssl already exists
+)
+cd openssl 
+git checkout %OPENSSL_VERSION%
+git pull origin %OPENSSL_VERSION%
 
-if [ ! -d "openssl-${VERSION}" ]; then
-    curl https://www.openssl.org/source/openssl-${VERSION}.tar.gz
-    tar -zxf openssl-${VERSION}.tar.gz
-fi
-
+perl Configure VC-WIN64A --prefix=%PREFIX% --openssldir=%PREFIX%
 
 nmake clean
 nmake
