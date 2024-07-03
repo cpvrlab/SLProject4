1ff54a82a3dbbbc6057eb33b8417bdfe57221f2d
Marino von Wattenwyl <m.v.wattenwyl@gmail.com>
2024-07-03 12:24:19 +0200

    Update the minimal app GUI

--- apps/app_minimal/AppMinimalMain.cpp
+++ apps/app_minimal/AppMinimalMain.cpp
@@ -3,14 +3,14 @@
  * \brief     Single file minimal full app with SLProject
  * \details   For more info on how to create a new app with SLProject see:
  *            https://github.com/cpvrlab/SLProject4/wiki/Creating-a-New-App
- *            For more info about App framework see: 
+ *            For more info about App framework see:
  *            https://cpvrlab.github.io/SLProject4/app-framework.html
  * \date      June 2024
  * \authors   Marino von Wattenwyl
  * \copyright http://opensource.org/licenses/GPL-3.0
  * \remarks   Please use clangformat to format the code. See more code style on
  *            https://github.com/cpvrlab/SLProject4/wiki/SLProject-Coding-Style
-*/
+ */
 
 #include <App.h>
 #include <AppCommon.h>
@@ -28,23 +28,23 @@ class AppMinimalScene : public SLScene
 {
 public:
     AppMinimalScene();
-    
+
     //! All scene specific assets have to be registered for async loading in here.
-    /*! @remark All scene sspecific assets have to be loaded async by overriding 
-    SLScene::registerAssetsToLoad and SLScene::assemble. Async loading and 
-    assembling means that it happens in a parallel thread and that in there are 
+    /*! @remark All scene sspecific assets have to be loaded async by overriding
+    SLScene::registerAssetsToLoad and SLScene::assemble. Async loading and
+    assembling means that it happens in a parallel thread and that in there are
     no OpenGL calls allowed. OpenGL calls are only allowed in the main thread.*/
     void registerAssetsToLoad(SLAssetLoader& al) override;
 
     //! After parallel loading of the assets the scene gets assembled in here.
-    /*! @remark All scene-specific assets have to be loaded async by overriding 
-    SLScene::registerAssetsToLoad and SLScene::assemble. Async loading and 
-    assembling means that it happens in a parallel thread and that in there 
-    are no OpenGL calls allowed. OpenGL calls are only allowed in the main 
-    thread. It is important that all object instantiations within 
-    SLScene::assemble do NOT call any OpenGL functions (gl*) because they happen 
-    in a parallel thread. All objects that get rendered have to do their 
-    initialization when they are used the first time during rendering in the 
+    /*! @remark All scene-specific assets have to be loaded async by overriding
+    SLScene::registerAssetsToLoad and SLScene::assemble. Async loading and
+    assembling means that it happens in a parallel thread and that in there
+    are no OpenGL calls allowed. OpenGL calls are only allowed in the main
+    thread. It is important that all object instantiations within
+    SLScene::assemble do NOT call any OpenGL functions (gl*) because they happen
+    in a parallel thread. All objects that get rendered have to do their
+    initialization when they are used the first time during rendering in the
     main thread.*/
     void assemble(SLAssetManager* am, SLSceneView* sv) override;
 
@@ -110,10 +110,13 @@ static void buildGui(SLScene* s, SLSceneView* sv)
 {
     if (ImGui::BeginMainMenuBar())
     {
-        if (ImGui::BeginMenu("File"))
+        if (ImGui::BeginMenu("View"))
         {
-            if (ImGui::MenuItem("Quit"))
-                slShouldClose(true);
+            if (ImGui::MenuItem("Reset Camera"))
+            {
+                sv->camera()->translation(2, 1, 3);
+                sv->camera()->lookAt(0, 0, 0);
+            }
 
             ImGui::EndMenu();
         }
